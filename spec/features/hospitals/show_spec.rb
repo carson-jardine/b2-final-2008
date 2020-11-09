07c23cb1b4b83a0f904840c76b85b654aff40150
Carson Jardine <carsonjardine@gmail.com>
2020-11-09 10:54:00 -0700

    Update hospital show spec to be more specific

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -9,16 +9,31 @@ describe 'As a visitor' do
       @bailey = @grey_sloan.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stamford University")
       @callie = @seaside.doctors.create!(name: "Calliope Torres", specialty: "Orthopedic Surgery", university: "Purdue University")
     end
+    describe "I see that hospital's information" do
+      it "I see that hospital's name with the number of doctors working there and the universities they attended" do
 
-    it "I see that hospital's name with the number of doctors working there and the universities they attended" do
+        visit "/hospitals/#{@grey_sloan.id}"
 
-      visit "/hospitals/#{@grey_sloan.id}"
+        expect(page).to have_content(@grey_sloan.name)
 
-      expect(page).to have_content(@grey_sloan.name)
-      expect(page).to have_content(@grey_sloan.doctor_count)
-      expect(page).to have_content(@grey_sloan.doctor_universities)
+        expect(page).to_not have_content(@seaside.name)
+      end
 
-      expect(page).to_not have_content(@seaside.name)
+      it "I see the number of doctors working at the hospital" do
+        visit "/hospitals/#{@grey_sloan.id}"
+
+        expect(page).to have_content(@grey_sloan.doctor_count)
+
+        expect(page).to_not have_content(@seaside.doctor_count)
+      end
+
+      it "I see a unique list of universities the hospital's doctors attended" do
+        visit "/hospitals/#{@grey_sloan.id}"
+
+        expect(page).to have_content(@grey_sloan.doctor_universities)
+
+        expect(page).to_not have_content(@seaside.doctor_universities)
+      end
     end
   end
 end
