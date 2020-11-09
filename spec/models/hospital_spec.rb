7b6e6799d9ef52da5edbf2e323b625424cc5ef64
Carson Jardine <carsonjardine@gmail.com>
2020-11-09 11:22:27 -0700

    Clean up test

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -11,15 +11,19 @@ RSpec.describe Hospital, type: :model do
       @seaside = Hospital.create!(name: "Seaside Health & Wellness")
       @meredith = @grey_sloan.doctors.create!(name: "Meredith Gray", specialty: "General Surgery", university: "Dartmouth College")
       @bailey = @grey_sloan.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stamford University")
-      @george = @grey_sloan.doctors.create!(name: "George O'Malley", specialty: "General Surgery", university: "Stamford University")
       @callie = @seaside.doctors.create!(name: "Calliope Torres", specialty: "Orthopedic Surgery", university: "Purdue University")
     end
     it "#doctor_count" do
-      expect(@grey_sloan.doctor_count).to eq(3)
+      expect(@grey_sloan.doctor_count).to eq(2)
       expect(@seaside.doctor_count).to eq(1)
+
+      expect(@grey_sloan.doctor_count).to_not eq(7)
+      expect(@seaside.doctor_count).to_not eq(2)
     end
 
     it "#doctor_universities" do
+      george = @grey_sloan.doctors.create!(name: "George O'Malley", specialty: "General Surgery", university: "Stamford University")
+
       expected = ["Stamford University", "Dartmouth College"]
       expect(@grey_sloan.doctor_universities.sort).to eq(expected.sort)
       expect(@grey_sloan.doctor_universities.count).to eq(2)
