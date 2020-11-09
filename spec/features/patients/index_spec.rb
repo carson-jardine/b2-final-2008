17f59db37ae43909c9a7ffa1852aca55980f74e8
Carson Jardine <carsonjardine@gmail.com>
2020-11-09 11:25:59 -0700

    Add additional hospital to test to make sure we get all patients

--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -4,12 +4,15 @@ describe 'As a visitor' do
   describe 'When I visit the patients index page' do
     before :each do
       @grey_sloan = Hospital.create!(name: "Grey Sloan Memorial")
+      @seaside = Hospital.create!(name: "Seaside Health & Wellness")
       @meredith = @grey_sloan.doctors.create!(name: "Meredith Gray", specialty: "General Surgery", university: "Dartmouth College")
       @bailey = @grey_sloan.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stamford University")
+      @callie = @seaside.doctors.create!(name: "Calliope Torres", specialty: "Orthopedic Surgery", university: "Purdue University")
+
       @denny = @meredith.patients.create!(name: "Denny Duquette", age: 38)
       @katie = @meredith.patients.create!(name: "Katie Bryce", age: 24)
       @zola = @bailey.patients.create!(name: "Zola Shepherd", age: 4)
-      @charlie = @bailey.patients.create!(name: "Charlie Hoynoski", age: 103)
+      @charlie = @callie.patients.create!(name: "Charlie Hoynoski", age: 103)
     end
 
     it "I see the names of all patients listed from oldest to youngest" do
